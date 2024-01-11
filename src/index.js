fd8f4e92bb3487f99fe48334bb4cdd1b0e74745c
Jobairniaz <120411194+Jobairniaz@users.noreply.github.com>
2024-01-11 23:38:50 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,99 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "intermediate",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git & GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "BackEnd",
+    level: "beginer",
+    color: "#008000",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="developer1.jpg" alt="Developer Profile" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jobair Niaz</h1>
+      <p>Frontend web developer. When not working, I like to travel.</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill }) {
+  return (
+    <div className="skill" style={{ backgroundColor: skill.color }}>
+      <span>{skill.skill}</span>
+      <span>
+        {skill.level === "advanced"
+          ? "💪"
+          : skill.level === "intermediate"
+          ? "👍"
+          : skill.level === "beginer"
+          ? "👶"
+          : ""}
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
